3fc4c224d041a58e89411d856fd2effe2b61f73b
ergo-ky <kylieshapeak@gmail.com>
2021-11-14 10:13:02 -0600

    Connecting Dapp functions and methods to the front end

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-Moralis.authenticate().then(function (user) {
-	console.log(user.get('ethAddress'))
-});
-console.log('hello blockians');
+/* Moralis init code */
+
+const serverUrl = "https://vrcbvnmimfvr.usemoralis.com:2053/server";
+const appId = "565KPZpHCNMtnzT0JBTleULYclemab6ZxPGVfdqm";
+
+Moralis.start({ serverUrl, appId });
+
+// Authentication code
+async function login() {
+	//magic moralis code, asks users to sign in via metamask if installed on browser
+	let user = Moralis.User.Current();
+
+	if(!user ){
+		user = await Moralis.authenticate({ signingMessage: "Log in"})
+		.then (function (user) {
+			console.log("loggen in user:", user);
+			console.log(user.get("ethAddress"));
+		})
+		.catch(function (error) {
+			console(error);
+		});
+	}
+}
